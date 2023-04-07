2c251efa548922c3d3f3a540d6f764bfd9d8f4fb
5h15h1r <shishir.agrahari14@gmail.com>
2023-04-07 14:42:50 +0530

    changed permission class

--- edtech/settings.py
+++ edtech/settings.py
@@ -85,8 +85,11 @@ TEMPLATES = [
     },
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
-    
+        'rest_framework.permissions.IsAuthenticated',
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
